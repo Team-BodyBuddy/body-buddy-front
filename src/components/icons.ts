cb2bf374d73263fa607db7d8eb392c6e87bad944
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2024-12-26 17:39:16 +0900

    경고 해결

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -1,5 +1,7 @@
-import { ReactComponent as HomeButton } from "../assets/images/Home_button.svg";
-import { ReactComponent as Character} from "../assets/images/potato_char.svg"
+import { ReactComponent as HomeButton } from "../assets/icons/Home_button.svg";
+import { ReactComponent as Character} from "../assets/icons/potato_char.svg";
+import { ReactComponent as MainCart } from "../assets/icons/main_cart.svg";
 
 export const HomeIcon = HomeButton; 
 export const PotatoImage = Character;
+export const Cart = MainCart;
