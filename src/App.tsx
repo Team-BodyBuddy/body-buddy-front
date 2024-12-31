e1a76df85ff1a971a728df478a10cd71b0193597
lee03876 <lee03876@catholic.ac.kr>
2024-12-31 09:51:40 +0900

    feat: Add login page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import theme from "./styles/theme";
 import { FluxProvider } from "./stores/FluxContext";
 import UserPage from "./pages/UserPage/UserPage";
 import AdminPage from "./pages/AdminPage/AdminPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 const App: React.FC = () => {
     return (
@@ -21,7 +22,7 @@ const App: React.FC = () => {
                     <Routes>
                         <Route path="/" element={<UserPage />} />
                         <Route path="/admin" element={<AdminPage />} />
-
+                        <Route path="/login" element={<LoginPage />} />
                     </Routes>
                 </Router>
             </ThemeProvider>
--- src/components/icons.ts
+++ src/components/icons.ts
@@ -2,6 +2,11 @@ import { ReactComponent as HomeButton } from "../assets/icons/Home_button.svg";
 import { ReactComponent as Character} from "../assets/icons/potato_char.svg";
 import { ReactComponent as MainCart } from "../assets/icons/main_cart.svg";
 
+import { ReactComponent as BodyBoddy } from "../assets/icons/Sign_up/BODY BUDDY.svg";
+
 export const HomeIcon = HomeButton; 
 export const PotatoImage = Character;
 export const Cart = MainCart;
+export const Logo = BodyBoddy;
+
+
