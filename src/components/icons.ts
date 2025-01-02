e1e7dee5d7b1c3f0a5074df0b361177c7cf4a835
LGY010011 <gylm1111pm@gmail.com>
2025-01-02 12:53:20 +0900

    feat: color 및 icon 추가

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -1,7 +1,11 @@
 import { ReactComponent as HomeButton } from "../assets/icons/Home_button.svg";
 import { ReactComponent as Character} from "../assets/icons/potato_char.svg";
 import { ReactComponent as MainCart } from "../assets/icons/main_cart.svg";
+import { ReactComponent as Profile } from "../assets/icons/default_profile.svg";
+import { ReactComponent as GHealth } from "../assets/icons/Green_health.svg";
 
 export const HomeIcon = HomeButton; 
 export const PotatoImage = Character;
 export const Cart = MainCart;
+export const DefaultProfile=Profile;
+export const GreenHealth=GHealth;
