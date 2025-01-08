5516f2fb2a2f964ad6ed38b173e16f2506b2da93
LGY010011 <gylm1111pm@gmail.com>
2025-01-09 00:41:39 +0900

    feat: Calendar 컴포넌트 구현

---
+++ src/components/Calendar/CalendarElement/Week/CalendarWeek.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import * as S from "./Styles";
+import { isSameMonth, isToday, format } from "date-fns";
+import CalendarDate from "../Date/CalendarDate";
+
+interface WeekRowProps {
+    week: Date[];
+    currentDate: Date;
+}
+
+const CalendarWeek: React.FC<WeekRowProps> = ({ week, currentDate }) => {
+    return (
+        <S.WeekRow>
+            {week.map((day, index) => (
+                <CalendarDate key={index} day={day} isCurrentMonth={isSameMonth(day, currentDate)} isToday={isToday(day)} />
+            ))}
+        </S.WeekRow>
+    );
+};
+
+export default CalendarWeek;
