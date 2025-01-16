51bc9f2580aac31dfc49236c180b4aae33e35b07
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2025-01-17 04:42:58 +0900

    refactor/folder-zustand
    테스트용 상태관리 파일 포함

---
+++ .history/src/zustand/stores/initialState_20250117043818.ts
@@ -0,0 +1,21 @@
+// 상태 초기값 타입 
+export const initialState = {
+  level: 4, // 현재 레벨
+  progress: 50, // 현재 경험치 게이지 퍼센트
+  points: 12765, // 포인트
+  metabolicRate: {
+      label: "기초대사량",
+      value: "1153 kcal",
+      trend: "up",
+      history: [1130, 1120, 1140, 1153],
+  },
+  weight: {
+      label: "몸무게",
+      value: "52kg",
+      trend: "down", 
+      history: [52.0, 54.0, 53.0, 52.0]
+  },
+};
+
+// 상태 타입
+export type StateType = typeof initialState;
