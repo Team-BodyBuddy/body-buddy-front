ee3998715b477f645a2d63ecaa09a2be9ec85f1b
lee03876 <lee03876@catholic.ac.kr>
2025-01-21 18:38:46 +0900

    style: px에서 rem으로 단위 변환

--- src/pages/Login_Signup/LoginPage/Styles.ts
+++ src/pages/Login_Signup/LoginPage/Styles.ts
@@ -7,47 +7,43 @@ export const Container = styled.div`
     width: 100vw;
     min-height: 100vh;
     max-height: 100vh;
-    margin: 0px 0px 0px 0px;
-    padding: 0px 0px 0px 0px;
+
     box-sizing: border-box;
     overflow-y: auto;
 
-    @media (max-width: 430px) {
+    @media (max-width: 26.875rem) {
         height: auto;
         background-color: #ffffff;
     }
 
-    @media (max-height: 932px) { 
-        max-height: calc(100vh - 10px);
+    @media (max-height: 58.25rem) {
+        max-height: calc(100vh - 0.625rem);
     }
 `;
 
-export const Intro = styled.div`
-    margin: 0px 0px 0px 0px;
-    padding: 0px 0px 0px 0px;
-`;
+export const Intro = styled.div``;
 
 export const IntroText = styled.div`
-    margin: 130px 0px 0px 33px;
+    margin: 8.125rem 0 0 2.0625rem;
 `;
 
 export const Title = styled.div`
-    margin: 22px 0px 0px 33px;
+    margin: 1.375rem 0 0 2.0625rem;
 `;
 
 export const CenterContainer = styled.div`
-    margin: 63px 0px 0px 36px;
-    width: 80%; 
-    height: 251px;
+    margin: 3.9375rem 0 0 2.25rem;
+    width: 80%;
+    height: 15.6875rem;
 `;
 
 export const ErrorMessage = styled.span`
     font-family: Pretendard;
-    font-size: 11px;
+    font-size: 0.6875rem;
     font-weight: 400;
-    line-height: 13.13px;
-    color: #FF4040;
-    position:relative;
-    top:15px;
-    left:155px;
+    line-height: 0.82rem;
+    color: #ff4040;
+    position: relative;
+    top: 0.9375rem;
+    left: 9.6875rem;
 `;
--- src/pages/Login_Signup/UserSignUpPage/Styles.ts
+++ src/pages/Login_Signup/UserSignUpPage/Styles.ts
@@ -7,45 +7,43 @@ export const Container = styled.div`
     width: 100vw;
     min-height: 100vh;
     max-height: 100vh;
-    margin: 0px 0px 0px 0px;
-    padding: 0px 0px 0px 0px;
     box-sizing: border-box;
     overflow-y: auto;
 
-    @media (max-width: 430px) {
+    @media (max-width: 26.875rem) {
         height: auto;
     }
 
-    @media (max-height: 932px) { 
-        max-height: calc(100vh - 10px);
+    @media (max-height: 58.25rem) {
+        max-height: calc(100vh - 0.625rem);
     }
 `;
 
 export const TopContainer = styled.div`
     position: sticky;
-    margin-top:3rem;
-    margin-bottom:2rem;
+    margin-top: 3rem;
+    margin-bottom: 2rem;
 `;
 
 export const TopBar = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: row;
     justify-content: center;
 `;
 
 export const Icon = styled.span`
-    position:relative;
-    top:-97px;
-    left:0px;
+    position: relative;
+    top: -3.35rem;
+    left: 0;
 `;
 
 export const Title = styled.div`
     display: flex;
     justify-content: center;
     font-family: Pretendard;
-    font-size: 20px;
+    font-size: 1.25rem;
     font-weight: 700;
-    line-height: 32px;
+    line-height: 2rem;
     letter-spacing: -0.02em;
     text-align: left;
     text-underline-position: from-font;
@@ -57,9 +55,8 @@ export const FindOptions = styled.div`
     justify-content: center;
     flex-direction: column;
     font-family: Pretendard;
-    /* font-size: 15px;
-    font-weight: 500; */
-    line-height: 29px;
+
+    line-height: 1.8125rem;
     letter-spacing: -0.02em;
     text-align: left;
     text-underline-position: from-font;
@@ -70,71 +67,66 @@ export const OptionsWrapper = styled.div`
     display: flex;
     justify-content: center;
     gap: 20%;
-
 `;
 
 export const LineWrapper = styled.span`
-    margin-top:15px;
-    border-bottom : 1px solid #40AD00;
-    margin-left:2rem;
-    margin-right:2rem;
+    margin-top: 0.9375rem;
+    border-bottom: 1px solid #40ad00;
+    margin-left: 2rem;
+    margin-right: 2rem;
 `;
 
 export const Trainer = styled.span`
-    display:flex;
-    gap:10px;
+    display: flex;
+    gap: 0.625rem;
 `;
 
 export const User = styled.span`
-    display:flex;
-    gap:10px;
+    display: flex;
+    gap: 0.625rem;
 `;
 
 export const Container1 = styled.div`
-    margin-left:32px;
-    margin-right:32px;
-    border-bottom : 1px solid #40AD00;
+    margin-left: 2rem;
+    margin-right: 2rem;
+    border-bottom: 1px solid #40ad00;
 `;
 
 export const Container2 = styled.div`
-    margin-left:32px;
-    margin-right:32px;
-    border-bottom : 1px solid #40AD00;
+    margin-left: 2rem;
+    margin-right: 2rem;
+    border-bottom: 1px solid #40ad00;
 `;
 
 export const Container3 = styled.div`
-    margin-left:32px;
-    margin-right:32px;
-    margin-bottom:150px;
+    margin-left: 2rem;
+    margin-right: 2rem;
+    margin-bottom: 9.375rem;
 `;
 
-export const Detail1 = styled.div`
-
-`;
+export const Detail1 = styled.div``;
 
 export const Detail2 = styled.div`
-    margin-bottom:20px;
+    margin-bottom: 1.25rem;
 `;
 
-export const Detail3 = styled.div`
-
-`;
+export const Detail3 = styled.div``;
 
 export const Detail4 = styled.div`
-    margin-bottom:20px;
+    margin-bottom: 1.25rem;
 `;
 
 export const NextContainer = styled.div`
-    margin-top:40px;
+    margin-top: 2.5rem;
 `;
 
 export const Header = styled.div`
-    margin-bottom: 10px;
-    margin-top: 20px;
+    margin-bottom: 0.625rem;
+    margin-top: 1.25rem;
     font-family: Pretendard;
-    font-size: 15px;
+    font-size: 0.9375rem;
     font-weight: 500;
-    line-height: 29px;
+    line-height: 1.8125rem;
     letter-spacing: -0.02em;
     text-align: left;
     text-underline-position: from-font;
@@ -143,42 +135,40 @@ export const Header = styled.div`
 `;
 
 export const Content = styled.div`
-    margin-bottom: 10px;
-    /* background-color:rgb(171, 159, 159); */
+    margin-bottom: 0.625rem;
 `;
 
 export const Box = styled.div`
-    display:flex;
-    gap:10px;
+    display: flex;
+    gap: 0.625rem;
 `;
 
 export const PasswordContent = styled.div`
-    display:flex;
-    flex-direction:column;
-    gap:10px;
+    display: flex;
+    flex-direction: column;
+    gap: 0.625rem;
 `;
 
 export const LocationContent = styled.div`
-    display:flex;
-    flex-direction:row;
-    gap:10px;
-    margin-bottom: 10px;
+    display: flex;
+    flex-direction: row;
+    gap: 0.625rem;
+    margin-bottom: 0.625rem;
 `;
 
 export const ContentWrapper = styled.div`
-    display:flex;
-    flex-direction:row;
-    gap:10px;
+    display: flex;
+    flex-direction: row;
+    gap: 0.625rem;
 `;
 
 export const Star = styled.span`
-    color: #40AD00;
+    color: #40ad00;
 `;
 
 export const BirthdayContent = styled.div`
-    margin-bottom: 10px;
-    gap:10px;
-    display:flex;
-    flex-direction:row;
+    margin-bottom: 0.625rem;
+    gap: 0.625rem;
+    display: flex;
+    flex-direction: row;
 `;
-
