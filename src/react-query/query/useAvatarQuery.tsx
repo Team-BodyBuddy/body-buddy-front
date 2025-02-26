549b458b63fd991c33027a5f729c44fd59714dc2
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-02-26 14:59:11 +0900

    feat/avatar-query

---
+++ src/react-query/query/useAvatarQuery.tsx
@@ -0,0 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPoints, getAvatarInfo, getInBody, getWeightHistory } from "../../apis/avatar";
+
+export const usePoints = (memberId: number) =>
+  useQuery({
+    queryKey: ["points", memberId],
+    queryFn: () => getPoints(memberId),
+    retry: 0,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+  });
+
+export const useAvatarInfo = (memberId: number) =>
+  useQuery({
+    queryKey: ["avatarInfo", memberId],
+    queryFn: () => getAvatarInfo(memberId),
+    retry: 0,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+  });
+
+export const useInBody = (memberId: number) =>
+  useQuery({
+    queryKey: ["inBody", memberId],
+    queryFn: () => getInBody(memberId),
+    retry: 0,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+  });
+
+export const useWeightHistory = (memberId: number) =>
+  useQuery({
+    queryKey: ["weightHistory", memberId],
+    queryFn: () => getWeightHistory(memberId),
+    retry: 0,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+  });
