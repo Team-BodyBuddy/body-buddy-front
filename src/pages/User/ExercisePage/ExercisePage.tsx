98e76750613511e506e27792c65015adefb93a8b
LGY010011 <gylm1111pm@gmail.com>
2025-03-03 19:59:20 +0900

    style: 운동관리페이지 ui

--- src/pages/User/ExercisePage/ExercisePage.tsx
+++ src/pages/User/ExercisePage/ExercisePage.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import TopNavigation from "../../../components/TopNavigation/TopNavigation";
+import * as S from "./Styles";
+import Calendar from "../../../components/Calendar/Calendar";
+import TodoElement from "../../../components/Todo/TodoElement";
+import { todoData } from "../../../mocks/todo-mock";
 
 const ExercisePage: React.FC = () => {
     const handleTabClick = (tab: string) => {
-      console.log(`${tab} 탭 클릭됨`);
+        console.log(`${tab} 탭 클릭됨`);
     };
     return (
         <>
-          <TopNavigation activeTab="운동관리" onTabClick={handleTabClick} />
-          <div>
-              <h1>운동 관리 페이지</h1>
-              <p>여기에 운동 관리와 관련된 콘텐츠를 추가하세요.</p>
-          </div>
+            <TopNavigation activeTab="운동관리" onTabClick={handleTabClick} />
+            <S.Container>
+                <S.ContentWrapper>
+                    <Calendar />
+                    <TodoElement data={todoData} />
+                </S.ContentWrapper>
+            </S.Container>
         </>
     );
 };
