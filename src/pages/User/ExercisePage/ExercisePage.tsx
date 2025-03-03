96a85e5dc88c8a01c85c933741831d2fc6b23397
LGY010011 <gylm1111pm@gmail.com>
2025-03-03 23:43:57 +0900

    feat: User 운동관리페이지 특정 달 조회 api 연결

--- src/pages/User/ExercisePage/ExercisePage.tsx
+++ src/pages/User/ExercisePage/ExercisePage.tsx
@@ -4,17 +4,36 @@ import * as S from "./Styles";
 import Calendar from "../../../components/Calendar/Calendar";
 import TodoElement from "../../../components/Todo/TodoElement";
 import { todoData } from "../../../mocks/todo-mock";
+import { useQuery } from "@tanstack/react-query";
+import { getMonthData } from "../../../apis/Calendar/calendarApi";
+import { format } from "date-fns";
 
 const ExercisePage: React.FC = () => {
     const handleTabClick = (tab: string) => {
         console.log(`${tab} 탭 클릭됨`);
     };
+
+    const currentMonth = format(new Date(), "yyyy-MM");
+
+    //첫 화면 렌더링 시 해당 달의 데이터를 가져와야 함
+    const {
+        data: userMonthData,
+        isLoading,
+        error,
+    } = useQuery({
+        queryKey: ["monthData", currentMonth],
+        queryFn: () => getMonthData(10, currentMonth),
+        retry: 0,
+    });
+
+    //날짜를 누르면 해당 날짜의 데이터를 가져오기
+
     return (
         <>
             <TopNavigation activeTab="운동관리" onTabClick={handleTabClick} />
             <S.Container>
                 <S.ContentWrapper>
-                    <Calendar />
+                    <Calendar calendarData={userMonthData} />
                     <TodoElement data={todoData} />
                 </S.ContentWrapper>
             </S.Container>
