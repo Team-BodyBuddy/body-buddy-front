9ac5f1066e6291a76a1d7a38c3f9ff39f08490b2
LGY010011 <gylm1111pm@gmail.com>
2025-03-03 19:31:57 +0900

    fix: 405 에러 getRankings url /api 추가

--- src/apis/RankingPage/rankingApi.ts
+++ src/apis/RankingPage/rankingApi.ts
@@ -6,7 +6,7 @@ const TempToken = process.env.REACT_APP_TEMP_TOKEN;
 
 export const getRankings = async (type: "gym" | "global"): Promise<RankingData> => {
     try {
-        const url = type === "global" ? "/rankings/global" : "/rankings/gym/1";
+        const url = type === "global" ? "/api/rankings/global" : "/api/rankings/gym/1";
         const response = await axiosInstance.get<ApiResponse<RankingData>>(url, {
             headers: {
                 Authorization: `Bearer ${TempToken}`,
@@ -23,6 +23,7 @@ export const getRankings = async (type: "gym" | "global"): Promise<RankingData> 
 export const getRankingUser = async (type: "gym" | "global"): Promise<RankingItem> => {
     try {
         const url = type === "global" ? "/api/rankings/user/1" : "/api/rankings/user/1/gym/1";
+        console.log(url);
         const response = await axiosInstance.get<ApiResponse<RankingItem>>(url, {
             headers: {
                 Authorization: `Bearer ${TempToken}`,
