d19bb9871304562bed6f9e645b3a8402e2ec995d
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-03-03 20:25:51 +0900

    fix/deco-apis

---
+++ src/apis/item/mocks.ts
@@ -0,0 +1,17 @@
+export const itemMockData = {
+    isSuccess: true,
+    code: "MOCK_DATA",
+    message: "This is mock data",
+    result: [
+        { id: 1, name: "", imagePath: "/assets/icons/Default_Item.svg", price: 1000, status: "ACTIVE" as "ACTIVE" },
+        { id: 2, name: "", imagePath: "/assets/icons/Default_Item.svg", price: 1200, status: "INACTIVE" as "INACTIVE" },
+        { id: 3, name: "", imagePath: "/assets/icons/Default_Item.svg", price: 1500, status: "ACTIVE" as "ACTIVE" },
+        { id: 4, name: "", imagePath: "/assets/icons/Default_Item.svg", price: 1800, status: "INACTIVE" as "INACTIVE" },
+        { id: 5, name: "", imagePath: "/assets/icons/Default_Item.svg", price: 2000, status: "ACTIVE" as "ACTIVE" },
+        { id: 6, name: "", imagePath: "/assets/icons/Default_Item.svg", price: 2200, status: "INACTIVE" as "INACTIVE" },
+        { id: 7, name: "", imagePath: "/assets/icons/Default_Item.svg", price: 2500, status: "ACTIVE" as "ACTIVE" },
+        { id: 8, name: "", imagePath: "/assets/icons/Default_Item.svg", price: 2700, status: "INACTIVE" as "INACTIVE" },
+        { id: 9, name: "", imagePath: "/assets/icons/Default_Item.svg", price: 3000, status: "ACTIVE" as "ACTIVE" },
+        { id: 10, name: "", imagePath: "/assets/icons/Default_Item.svg", price: 3200, status: "INACTIVE" as "INACTIVE" },
+    ],
+};
