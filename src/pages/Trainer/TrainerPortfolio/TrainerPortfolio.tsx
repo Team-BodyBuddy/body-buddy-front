cc5534af0a8ef746ac4f3f9b2a1692465256cef4
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-03-05 01:16:00 +0900

    feat/badges

--- src/pages/Trainer/TrainerPortfolio/TrainerPortfolio.tsx
+++ src/pages/Trainer/TrainerPortfolio/TrainerPortfolio.tsx
@@ -1,30 +1,29 @@
 import React, { useState } from "react";
+import { useTrainerBadges } from "../../../react-query/query/badges/useBadgesQuery";
 import BadgeSection from "../../../components/BadgeSection/BadgeSection";
 import EditableList from "../../../components/EditableList/EditableList"; 
 import MemberListSection from "../../../components/MemberListSection/MemberListSection";
 import * as S from "./Styles";
 import EditForm from "../../../components/EditForm/EditForm";
-import TrainerTopNavi from "./../../../components/TrainerTopNavi/TrainerTopNavi"
+import TrainerTopNavi from "./../../../components/TrainerTopNavi/TrainerTopNavi";
 import { CheckPort } from "../../../components/icons";
 
 const TrainerPortfolio: React.FC = () => {
     const [isEditingList, setIsEditingList] = useState(false); 
     const [isEditingRegion, setIsEditingRegion] = useState(false); 
     const [selectedItem, setSelectedItem] = useState<string | null>(null); 
-
-    // 테스트 데이터터
     const aboutItems = Array.from({ length: 10 }, (_, index) => `포트폴리오 수정하기 ${index + 1}`);
+    const { data, isLoading } = useTrainerBadges();
 
     const handleItemClick = (item: string) => {
-        setSelectedItem(item); // 선택된 아이템 상태 업데이트
+        setSelectedItem(item); 
         console.log(`선택된 아이템: ${item}`);
     };
 
-    // 저장 버튼 클릭 핸들러
     const handleSave = () => {
         if (selectedItem) {
             console.log(`저장 완료: ${selectedItem}`);
-            setIsEditingList(false); // 저장 후 창 닫기
+            setIsEditingList(false);
         } else {
             console.log("아이템을 선택해주세요.");
         }
@@ -74,9 +73,9 @@ const TrainerPortfolio: React.FC = () => {
                         </S.SectionHeader>
                     </S.EditableSection>
                 )}
-                <BadgeSection 
-                    badges={["어떤 뱃지", "어떤 뱃지", "어떤 뱃지"]} 
-                />
+
+                {isLoading ? <p>Loading badges...</p> : <BadgeSection badges={data?.result.map(badge => badge.badgeName) || []} />}
+                
                 <MemberListSection />
             </S.DetailContainer>
         </>
