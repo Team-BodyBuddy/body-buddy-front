c8554964effa735ef6f052e6adb95fcc7af0fa22
LGY010011 <gylm1111pm@gmail.com>
2025-03-04 18:38:34 +0900

    feat: POST /evaluation/set API 연결

---
+++ src/apis/TodayScore/todayScoreApi.ts
@@ -0,0 +1,16 @@
+import axiosInstance from "../axiosInstance";
+
+const TempToken = process.env.REACT_APP_TEMP_TOKEN;
+
+export const postDayScore = async (memberId: number, date: string, status: "BAD" | "SOSO" | "GOOD") => {
+    const reponse = await axiosInstance.post(
+        "/api/evaluation/set",
+        { memberId, date, status },
+        {
+            headers: {
+                Authorization: `Bearer ${TempToken}`,
+            },
+        }
+    );
+    return reponse.data;
+};
--- src/pages/User/ExercisePage/ExercisePage.tsx
+++ src/pages/User/ExercisePage/ExercisePage.tsx
@@ -4,16 +4,21 @@ import * as S from "./Styles";
 import Calendar from "../../../components/Calendar/Calendar";
 import TodoElement from "../../../components/Todo/TodoElement";
 import { todoData } from "../../../mocks/todo-mock";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getMonthData } from "../../../apis/Calendar/calendarApi";
 import { format } from "date-fns";
+import TodayScore from "../../../components/TodayScore/TodayScore";
+import { postDayScore } from "../../../apis/TodayScore/todayScoreApi";
 
 const ExercisePage: React.FC = () => {
+    const memberId = 10; // 임시 memberId
+
     const handleTabClick = (tab: string) => {
         console.log(`${tab} 탭 클릭됨`);
     };
 
     const currentMonth = format(new Date(), "yyyy-MM");
+    const today = format(new Date(), "yyyy-MM-dd");
 
     //첫 화면 렌더링 시 해당 달의 데이터를 가져와야 함
     const {
@@ -22,12 +27,27 @@ const ExercisePage: React.FC = () => {
         error,
     } = useQuery({
         queryKey: ["monthData", currentMonth],
-        queryFn: () => getMonthData(10, currentMonth),
+        queryFn: () => getMonthData(memberId, currentMonth),
         retry: 0,
     });
 
     //날짜를 누르면 해당 날짜의 데이터를 가져오기
     //루틴 데이터
+
+    //오늘의 평가 등록하기
+    const { mutate: submitTodayScore } = useMutation({
+        mutationFn: (status: "BAD" | "SOSO" | "GOOD") => postDayScore(memberId, today, status),
+        onSuccess: () => {
+            console.log("오늘의 평가 제출 성공");
+        },
+        onError: (err) => {
+            console.error("오늘의 평가 제출 실패:", err);
+        },
+    });
+
+    const handleClick = (score: "BAD" | "SOSO" | "GOOD") => {
+        submitTodayScore(score);
+    };
 
     if (isLoading) {
         return <div>로딩 중...</div>;
@@ -44,6 +64,7 @@ const ExercisePage: React.FC = () => {
                 <S.ContentWrapper>
                     <Calendar calendarData={userMonthData} />
                     <TodoElement data={todoData} />
+                    <TodayScore onClick={handleClick} memberId={memberId} date={today} />
                 </S.ContentWrapper>
             </S.Container>
         </>
