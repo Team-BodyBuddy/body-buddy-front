b773fa4a738f3947912aa9bbb9fc3d6dc336668c
LGY010011 <gylm1111pm@gmail.com>
2025-03-06 17:23:55 +0900

    feat: GET /routine/memberId/date API

--- src/apis/Routine/routineApi.ts
+++ src/apis/Routine/routineApi.ts
@@ -1,3 +1,5 @@
+import { ApiResponse } from "../../entity/ApiResponse";
+import { RoutineItem } from "../../entity/RoutineEntity";
 import axiosInstance from "../axiosInstance";
 
 const TempToken = process.env.REACT_APP_TEMP_TOKEN;
@@ -14,4 +16,14 @@ export const postRoutine = async (routineData: { memberId: number; date: string;
         }
     );
     return reponse.data.result;
+};
+
+export const getRoutineData = async (memberId: number, date: string) => {
+    const response = await axiosInstance.get<ApiResponse<RoutineItem[]>>(`/api/routine/${memberId}/${date}`, {
+        headers: {
+            Authorization: `Bearer ${TempToken}`,
+        },
+    });
+
+    return response.data.result;
 };
