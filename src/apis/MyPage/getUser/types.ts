ee99d11eb16290477413d56abb33964b8a741920
KIM JEONGMIN <wjdals030328@catholic.ac.kr>
2025-03-07 16:55:35 +0900

    feat:user 리액트쿼리

--- src/apis/MyPage/getUser/types.ts
+++ src/apis/MyPage/getUser/types.ts
@@ -1,16 +1,16 @@
 export type getUser = {
-    isSuccess: true;
-    code: "COMMON200";
-    message: "성공입니다.";
+    isSuccess: boolean;
+    code: string;
+    message: string;
     result: {
-        memberId: 13;
-        nickname: "nemo";
-        avatarLevel: 1;
-        realName: "김정민";
-        gender: "MALE";
-        birthday: "2003-03-28";
-        region: "GANGNAM";
-        gymName: "Kongo";
-        heightWeight: "163cm / 0kg";
+        memberId: number;
+        nickname: string;
+        avatarLevel: number;
+        realName: string;
+        gender: string;
+        birthday: string;
+        region: string;
+        gymName: string;
+        heightWeight: string;
     };
 };
