ee32b322c1b3234e7885bd432206a8d488b67d34
LGY010011 <gylm1111pm@gmail.com>
2025-03-09 23:33:28 +0900

    refactor: useRoutineMutation에서 updateMonthDataIndicator 함수 사용
    루틴 등록 및 삭제 성공  시 데이터를 수정하여 불필요한 요청 제거

--- src/react-query/mutation/useRoutineMutation.ts
+++ src/react-query/mutation/useRoutineMutation.ts
@@ -1,14 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postRoutine, removeRoutine, toggleRoutine } from "../../apis/Routine/routineApi";
+import { updateMonthDataIndicator } from "../../utils/updateMonthData";
 
 export const useAddRoutineMutation = () => {
     const queryClient = useQueryClient();
 
     return useMutation({
         mutationFn: postRoutine,
-        onSuccess: (variables) => {
-            queryClient.invalidateQueries({ queryKey: ["monthData", variables.date.substring(0, 7)] });
-            queryClient.invalidateQueries({ queryKey: ["routineData", variables.date] });
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: ["routineData", data.date] });
+            queryClient.setQueryData(["monthData", data.date.slice(0, 7)], (oldData: any) => {
+                return updateMonthDataIndicator(oldData, data);
+            });
         },
         onError: (error: any) => {
             console.error("Error adding routine:", error);
@@ -21,9 +24,11 @@ export const useRemoveRoutineMutation = () => {
 
     return useMutation({
         mutationFn: removeRoutine,
-        onSuccess: (variables) => {
-            queryClient.invalidateQueries({ queryKey: ["monthData", variables.date.substring(0, 7)] });
-            queryClient.invalidateQueries({ queryKey: ["routineData", variables.date] });
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: ["routineData", data.date] });
+            queryClient.setQueryData(["monthData", data.date.slice(0, 7)], (oldData: any) => {
+                return updateMonthDataIndicator(oldData, data);
+            });
         },
         onError: (error: any) => {
             console.error("Error removing routine:", error);
