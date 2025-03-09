8ab02ebbdc4ecf2e73608ee98117f547c0165a18
LGY <gylm1111pm@gmail.com>
2025-03-09 22:14:22 +0900

    chore: routineApi 디버깅용 console.log 제거

--- src/apis/Routine/routineApi.ts
+++ src/apis/Routine/routineApi.ts
@@ -5,7 +5,6 @@ import axiosInstance from "../axiosInstance";
 const TempToken = process.env.REACT_APP_TEMP_TOKEN;
 
 export const postRoutine = async (routineData: { memberId: number; date: string; routineType: string; name: string }) => {
-    console.log(routineData);
     const reponse = await axiosInstance.post(
         "/api/routine/add",
         { ...routineData },
@@ -38,7 +37,6 @@ export const getRoutineData = async (memberId: number, date: string) => {
 };
 
 export const toggleRoutine = async (routineId: number) => {
-    console.log(`${TempToken}`);
     const response = await axiosInstance.patch(
         `/api/routine/${routineId}/toggle`,
         {},
